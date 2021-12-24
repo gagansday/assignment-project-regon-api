f6c9b4af322a1ed18ea137b0c45e1f0e1038c64a
Gagandeep Singh <gagansday@gmail.com>
2021-12-24 16:03:56 +0530

    enable queue

--- app/Mail/NeedVerificationMail.php
+++ app/Mail/NeedVerificationMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NeedVerificationMail extends Mailable
+class NeedVerificationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
